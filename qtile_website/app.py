1a14a204c18632328dc5142d9f7e20210520afbb
dmpayton <derek.payton@gmail.com>
2012-04-10 22:16:28 -0700

    I <3 CherryPy, but it has issues on Heroku. Begrudgingly switch to Flask

--- qtile_website/app.py
+++ qtile_website/app.py
@@ -1,48 +1,29 @@
-#!/usr/bin/env python
-
-import cherrypy
-import jinja2
+import flask
 import os
-from jinja2 import Environment, FileSystemLoader, TemplateNotFound
+from jinja2 import TemplateNotFound
 from pygments_extension import PygmentsExtension
 
-## Setup our jinja template loader
-root = os.path.dirname(os.path.abspath(__file__))
-env = Environment(
-    loader=FileSystemLoader(os.path.join(root, 'templates')),
-    extensions=[PygmentsExtension])
+app = flask.Flask(__name__)
+app.jinja_env.add_extension(PygmentsExtension)
 
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>/')
+def qtile_dot_org(path):
+    ## Redirect the favicon to the proper file
+    if path == 'favicon.ico':
+        return flask.redirect('/static/img/favicon.ico')
 
-## A simple app that serves templates based on the URL path
-class QtileDotOrg(object):
-    @cherrypy.expose
-    def default(self, *path, **kwargs):
-        path = '/'.join(path) or 'index'
-        try:
-            template = env.get_template('{0}.html'.format(path))
-        except TemplateNotFound:
-            template = env.get_template('404.html')
-        return template.render()
+    ## Render the appropriate template based on the URL path
+    try:
+        template = 'pages/{0}.html'.format(path or 'index')
+        response = flask.render_template(template)
+    except TemplateNotFound:
+        ## No template == 404
+        response = flask.render_template('404.html')
+    return response
 
-## Monkeypatch for Heroku
-## See: https://bitbucket.org/cherrypy/cherrypy/issue/1100/cherrypy-322-gives-engine-error-when
-from cherrypy.process import servers
-def fake_wait_for_occupied_port(host, port): return
-servers.wait_for_occupied_port = fake_wait_for_occupied_port
-
-## Set the config and it's time to rock 'n roll!
-cherrypy.config.update({
-    'server.socket_host': '0.0.0.0',
-    'server.socket_port': int(os.environ.get('PORT', 8080)),
-    })
-
-cherrypy.quickstart(QtileDotOrg(), config={
-    '/favicon.ico': {
-        'tools.staticfile.on': True,
-        'tools.staticfile.filename': os.path.join(root, 'static/img/favicon.ico'),
-        },
-    '/static': {
-        'tools.staticdir.on': True,
-        'tools.staticdir.dir': os.path.join(root, 'static'),
-        }
-    })
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000)),
+    )
