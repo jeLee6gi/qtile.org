a79cfeb01674050e2cd50765bf8db7b9659006cd
dmpayton <derek.payton@gmail.com>
2012-04-10 23:01:07 -0700

    Compile CSS with LessCSS, display compiled CSS when on Heroku

--- qtile_website/app.py
+++ qtile_website/app.py
@@ -6,6 +6,8 @@ from pygments_extension import PygmentsExtension
 app = flask.Flask(__name__)
 app.jinja_env.add_extension(PygmentsExtension)
 
+debug = not os.environ.get('ON_HEROKU')
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>/')
 def qtile_dot_org(path):
@@ -16,14 +18,15 @@ def qtile_dot_org(path):
     ## Render the appropriate template based on the URL path
     try:
         template = 'pages/{0}.html'.format(path or 'index')
-        response = flask.render_template(template)
+        response = flask.render_template(template, debug=debug)
     except TemplateNotFound:
         ## No template == 404
-        response = flask.render_template('404.html')
+        response = flask.render_template('404.html', debug=debug)
     return response
 
 if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
         port=int(os.environ.get('PORT', 5000)),
+        debug=debug
     )
